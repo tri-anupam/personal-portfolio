40b3a16af3364b2a2f71deaadb621992c8cee730
Anupam Tripathi <arvindtri.70@gmail.com>
2023-07-10 23:03:58 +0530

    SectionWrapper HOC is completed for about section

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -12,6 +12,9 @@ const SectionWrapper = (Component, idName) => function HOC() {
         viewport={{once:true,amout:0.25}}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
     >
+      <span className="hash-span" id={idName}>
+        &nbsp;
+      </span>
         <Component/>
     </motion.section>
   )
