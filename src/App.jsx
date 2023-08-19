71d4c33d1f70f05c15913c8f31cb25ecd26ebe01
Anupam Tripathi <arvindtri.70@gmail.com>
2023-08-19 17:18:25 +0530

    changes in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works} from './components';
-
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+} from "./components";
 const App = () => {
-  
   return (
-   <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center text-red-600">
-        <Navbar/>
-        <Hero/>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center text-red-600">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
-      <About/>
-      <Experience/>
-      <Tech/>
-      <Works/>
-      <Feedbacks/>
-      <div className="relative z-0">
-        <Contact/>
-        <StarsCanvas/>
-      </div>
-    </div>
-   </BrowserRouter>
-  )
-}
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
